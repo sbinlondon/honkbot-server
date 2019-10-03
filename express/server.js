a7a534476a32ace5a55667ba0b4999ad8dba4a63
Kate Beard <asouthernbellein@gmail.com>
2019-10-03 11:50:32 +0100

    Add honk endpoint

--- express/server.js
+++ express/server.js
@@ -6,6 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
@@ -13,6 +14,12 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+router.get('/honk', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.json({honk: "honk"});
+  res.end();
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
