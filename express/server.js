ebb17ad758a922d9adcca81ee950dc6b02a90e5b
Kate Beard <asouthernbellein@gmail.com>
2019-10-03 12:10:12 +0100

    req.body.challenge

--- express/server.js
+++ express/server.js
@@ -16,7 +16,11 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 router.get('/honk', (req, res) => res.json({honk: "honk"}));
-router.post('/honk', (req, res) => res.send(req.query.challenge);
+router.post('/honk', (req, res) => {
+           const challenge = req.body.challenge
+           res.writeHead(200, { 'Content-Type': 'text/plain' });
+           res.send(`${challenge}`);
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
